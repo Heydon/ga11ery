43961680ea4a066a7fa628d2d724506f867ed3c9
Heydon <heydon@heydonworks.com>
2020-04-09 15:35:10 +0100

    nav links fix

--- src/filters/navLinks.js
+++ src/filters/navLinks.js
@@ -15,7 +15,7 @@ module.exports = function navLinks(collection, page) {
 
   const prev = collection[pageIndex - 1] || null;
   const next = collection[pageIndex + 1] || null;
-  const first = collection[0].url !== page ? collection[0].url : null;
+  const first = collection[0].url !== page.url ? collection[0] : null;
   const last = collection.slice(-1)[0].url !== page.url ? collection.slice(-1)[0] : null;
   const others = collection.filter(item => item.url !== page.url);
   const random = others[Math.floor(Math.random() * others.length)] || null;
