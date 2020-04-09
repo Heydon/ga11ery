71e7dc298da9de73e5959c184c015d58d818c4a2
Heydon <heydon@heydonworks.com>
2020-04-09 11:19:40 +0100

    randomness enhanced + workflow removed

--- src/filters/navLinks.js
+++ src/filters/navLinks.js
@@ -1,3 +1,12 @@
+const shuffle = (array) => {
+  const clone = array.slice(0);
+  for (let i = clone.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [clone[i], clone[j]] = [clone[j], clone[i]];
+  }
+  return clone;
+}
+
 module.exports = function navLinks(collection, page) {
   const pageIndex = collection.findIndex(item => item.url === page.url);
   const matches = (a, b) => {
@@ -6,16 +15,18 @@ module.exports = function navLinks(collection, page) {
 
   const prev = collection[pageIndex - 1] || null;
   const next = collection[pageIndex + 1] || null;
-  const first = collection[0].url !== page.url ? collection[0] : null;
+  const first = collection[0].url !== page ? collection[0].url : null;
   const last = collection.slice(-1)[0].url !== page.url ? collection.slice(-1)[0] : null;
   const others = collection.filter(item => item.url !== page.url);
   const random = others[Math.floor(Math.random() * others.length)] || null;
+  const randomFive = shuffle(others).slice(0, 5).map(other => other.url).join(',');
 
   return {
     prev,
     next,
     first,
     last,
-    random
+    random,
+    randomFive
   }
 }
